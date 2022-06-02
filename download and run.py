f909c66e7b2b732daeac1345b94b378c9bae4b39
tygoz <81298529+tygoz@users.noreply.github.com>
2022-06-02 13:18:51 +0200

    update

--- download and run.py
+++ download and run.py
@@ -1,5 +1,23 @@
-import wget
+
 import os
+path = os.environ.get('HOMEPATH')
+imports = ['keyboard','mouse','random','time','wget']
+imports2 = ['keyboard-','mouse-','random-','time-','wget-']
+a = -1
+listy = str(os.listdir(f'{path}/AppData/Local/Programs/Python/Python39/Lib/site-packages/'))
+while not 3 == a:
+    b = 0
+    a = a + 1
+    if imports[a] in listy:
+    else:
+        b = 1
+    if imports2[a] in listy:
+    else:
+        b = 1
+    if b == 1:
+        b = 0
+        os.system(f"pip install {imports[a]}")
+import wget
 remote_url = 'https://raw.githubusercontent.com/tygoz/pyclicker/main/autoclicker.py'
 local_file = 'click.txt'
 wget.download(remote_url, local_file)
