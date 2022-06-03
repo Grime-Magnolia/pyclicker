d3a265e215885cd878d220005969630fb0ede221
tygoz <81298529+tygoz@users.noreply.github.com>
2022-06-03 06:41:36 +0200

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,9 +1,12 @@
 import os
 path = os.environ.get('LOCALAPPDATA')
-imports = ['keyboard','mouse','random','time','wget']
-imports2 = ['keyboard-','mouse-','random-','time-','wget-']
+imports = ['keyboard','mouse','random','time','wget', 'requests']
+imports2 = ['keyboard-','mouse-','random-','time-','wget-','requests-']
 a = -1
-listy = str(os.listdir(f'{path}/Programs/Python/Python39/Lib/site-packages/'))
+try:
+    listy = str(os.listdir(f'{path}\Programs\Python\Python39\Lib\site-packages'))
+except:
+    listy = str(os.listdir(f'{path}\Programs\Python\Python310\Lib\site-packages'))
 while not 3 == a:
     b = 0
     a = a + 1
@@ -18,13 +21,22 @@ while not 3 == a:
     if b == 1:
         b = 0
         os.system(f"pip install {imports[a]}")
-import wget
 remote_url = 'https://raw.githubusercontent.com/tygoz/pyclicker/main/autoclicker.py'
 local_file = 'click.txt'
-wget.download(remote_url, local_file)
-f = open("click.txt", "r")
-file = f.read()
-f.close()
-os.remove("click.txt")
+
+try:
+    import wget
+    wget.download(remote_url, local_file)
+    f = open(local_file, "r")
+    file = f.read()
+    f.close()
+    os.remove(local_file)
+except:
+    import requests
+    response = requests.get(remote_url)
+    f = open(local_file, "wb").write(response.content)
+    file = f.read()
+    f.close()
+    os.remove(local_file)
 print('')
 exec(file)
