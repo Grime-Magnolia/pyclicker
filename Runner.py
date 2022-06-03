aa62da31f8218cea9d670ccc8badc654b71843eb
tygoz <81298529+tygoz@users.noreply.github.com>
2022-06-03 10:37:12 +0200

    update will now random generate name of file and made it easier for the reader to read
    update will now random generate name of file and made it easier for the reader to read

--- Runner.py
+++ Runner.py
@@ -1,42 +1,50 @@
 import os
+import random
+import string
+'--------------------------'
+letters = string.ascii_lowercase
+''.join(random.choice(letters) for i in range(10))
 path = os.environ.get('LOCALAPPDATA')
-imports = ['keyboard','mouse','random','time','wget', 'requests']
-imports2 = ['keyboard-','mouse-','random-','time-','wget-','requests-']
+imports = ['keyboard','mouse','random','time','wget',]
+imports2 = ['keyboard-','mouse-','random-','time-','wget-']
 a = -1
+remote_url = 'https://raw.githubusercontent.com/tygoz/pyclicker/main/autoclicker.py'
+local_file = '{''.join(random.choice(letters) for i in range(10))}.txt'
+'--------------------------'
 try:
-    listy = str(os.listdir(f'{path}\Programs\Python\Python39\Lib\site-packages'))
+    listy = str(os.listdir(f'{path}/Programs/Python/Python310/Lib/site-packages'))
 except:
-    listy = str(os.listdir(f'{path}\Programs\Python\Python310\Lib\site-packages'))
+    listy = str(os.listdir(f'{path}/Programs/Python/Python39/Lib/site-packages'))
 while not 3 == a:
     b = 0
     a = a + 1
-    if imports[a] in listy:
-        pass
-    else:
+    if not imports[a] in listy:
         b = 1
-    if imports2[a] in listy:
-        pass
-    else:
+    if not imports2[a] in listy:
         b = 1
     if b == 1:
         b = 0
         os.system(f"pip install {imports[a]}")
-remote_url = 'https://raw.githubusercontent.com/tygoz/pyclicker/main/autoclicker.py'
-local_file = 'click.txt'
-
-try:
-    import wget
-    wget.download(remote_url, local_file)
-    f = open(local_file, "r")
-    file = f.read()
-    f.close()
-    os.remove(local_file)
-except:
-    import requests
-    response = requests.get(remote_url)
-    f = open(local_file, "wb").write(response.content)
-    file = f.read()
-    f.close()
-    os.remove(local_file)
+import wget
+wget.download(remote_url, local_file)
+f = open(local_file, "r")
+file = f.read()
+f.close()
+os.remove(local_file)
 print('')
 exec(file)
+
+'''
+if the file doesn't download replace it with the code below
+
+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+import requests
+response = requests.get(remote_url)
+f = open(local_file, "wb").write(response.content)
+file = f.read()
+f.close()
+os.remove(local_file)
+
+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+'''
