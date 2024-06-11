e61d1fc56c58027b056ff9739efd71056ba8bbd5
tygoz <81298529+tygoz@users.noreply.github.com>
2024-06-11 08:38:55 +0000

    Update autoclicker.py
    check if linux has root perms when running else it wont run and except

--- autoclicker.py
+++ autoclicker.py
@@ -1,4 +1,9 @@
-import keyboard ,mouse ,random, time
+import keyboard ,mouse ,random, time, platform
+try:
+    os.rename('/etc/foo', '/etc/bar')
+except IOError as e:
+    if e[0] == errno.EPERM:
+       sys.exit("You need root permissions to do this, laterz!")
 if(input('R or L:\n') == "R"):
     button = "right"
 else:
