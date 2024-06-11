f6753fae1fd69c1178270f474be6c5bdef52331c
tygoz <81298529+tygoz@users.noreply.github.com>
2024-06-11 08:39:30 +0000

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -3,7 +3,7 @@ try:
     os.rename('/etc/foo', '/etc/bar')
 except IOError as e:
     if e[0] == errno.EPERM:
-       sys.exit("You need root permissions to do this, laterz!")
+       sys.exit("You need root permissions to run this on linux!")
 if(input('R or L:\n') == "R"):
     button = "right"
 else:
